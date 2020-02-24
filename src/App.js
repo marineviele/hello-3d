d45f8d5ab0c154ebed5a6ac1420ea7dad72dbc42
“Marina <>
2020-02-24 16:55:08 +0000

    WIP: integrate three.js library and cube example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import * as THREE from 'three';
+
 import Info from './Info';
 import Hello from './Hello';
 
-function App() {
-  const [displayInfo, setDisplayInfo] = useState(false);
 
-  return (
+class App extends Component {
+  state = {
+    displayInfo: false
+  };
+
+  componentDidMount() {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    var renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild( renderer.domElement );
+    // === Intergrate in React  ===
+    this.mount.appendChild(renderer.domElement);
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0xf8dd74 });
+    var cube = new THREE.Mesh(geometry, material);
+
+    scene.add(cube);
+    camera.position.z = 5;
+
+    var animate = function() {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.005;
+      cube.rotation.y += 0.005;
+      renderer.render(scene, camera);
+    };
+    animate();
+    // === THREE.JS EXAMPLE CODE END ===
+  }
+
+  render() {
+    return (
       <div>
-          {displayInfo ? (<Info/>) : (<Hello/>)}
+        {this.state.displayInfo ? <Info /> : <Hello />}
+        <div ref={ref => (this.mount = ref)} id="threeD"/>
       </div>
-  )
+    );
+  }
 }
 
 export default App;
